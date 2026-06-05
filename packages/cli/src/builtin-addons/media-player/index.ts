5806941f2627507f202c893d05223291dbcf520d
Sergio Carracedo <hi@sergiocarracedo.es>
2026-06-05 11:08:11 +0200

    feat(44-01): register media-mute and media-volume buttons in addon

--- packages/cli/src/builtin-addons/media-player/index.ts
+++ packages/cli/src/builtin-addons/media-player/index.ts
@@ -1,10 +1,16 @@
 import { SIRENO_ADDON_API_VERSION, type SirenoAddon } from '../../addon/api.js'
 
+import { builtinMediaMuteButton } from './buttons/media-mute.js'
+import { builtinMediaVolumeButton } from './buttons/media-volume.js'
 import { createMediaPlayerButton } from './media-player-button.js'
 
 const mediaPlayerAddon: SirenoAddon = {
   apiVersion: SIRENO_ADDON_API_VERSION,
-  buttons: [createMediaPlayerButton()],
+  buttons: [
+    createMediaPlayerButton(),
+    builtinMediaMuteButton,
+    builtinMediaVolumeButton,
+  ],
   name: 'media-player',
 }
 
